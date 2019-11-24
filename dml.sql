7d4b9b3192aa59734b421cfba21211eed7f224a9
wongte <56995724+wongte@users.noreply.github.com>
2019-11-24 21:15:14 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+INSERT INTO student(id, name, age, sex) VALUES(001, 'Colin', 18, 'female');
 -- Revise record
-
+UPDATE student SET sex='male' WHERE id=001;
 --  Delete record
-
+DELETE FROM student WHERE name='Colin';
 -- Search record
+SELECT * FROM student WHERE sex='male';
